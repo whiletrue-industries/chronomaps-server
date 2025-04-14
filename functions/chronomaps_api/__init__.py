15e5c26200c9d0fc29eafa1b6720216e1895d593
Adam Kariv <adam.kariv@gmail.com>
2025-04-14 21:15:40 +0300

    Improve list items endpoint

--- functions/chronomaps_api/__init__.py
+++ functions/chronomaps_api/__init__.py
@@ -75,7 +75,7 @@ def get_items(workspace):
     items = db.collection(workspace).order_by(order_by, direction=direction).stream()
     items = (dict(**doc.to_dict(), id=doc.id) for doc in items)
     items_metadata = (
-        dict(**item.get("metadata", {}), _id=item.id) for item in items if item['id'][0] != "."
+        dict(**item.get("metadata", {}), _id=item['id']) for item in items if item['id'][0] != "."
     )
     paginated_items = list(islice(items_metadata, page * page_size, (page + 1) * page_size))
     return paginated_items, 200
